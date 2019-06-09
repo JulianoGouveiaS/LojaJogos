c4418a9021993995c2f982f655a124bdd875f39d
juliano <juliano_gouveia1@hotmail.com>
2019-06-09 16:09:53 -0300

    commit 5

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace LojadeJogo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // test
+            // test lohan 
         }
     }
 }
