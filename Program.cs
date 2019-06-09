7779651716d9baef3d34ed15ff41ec148d6fa48e
Vitor Claudio <vitorcldeos@gmail.com>
2019-06-09 16:06:55 -0300

    tomaaa

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace LojadeJogo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //
         }
     }
 }
