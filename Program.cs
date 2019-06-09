3f5ca69972e968cfa051f599410d965c7af8bc3e
Vitor Claudio <vitorcldeos@gmail.com>
2019-06-09 17:13:59 -0300

    MySql Script

--- Program.cs
+++ Program.cs
@@ -17,7 +17,137 @@ namespace LojadeJogo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // test lohan lgthg
+            /* -- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema LojaDeJogos
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema LojaDeJogos
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `LojaDeJogos` DEFAULT CHARACTER SET utf8 ;
+USE `LojaDeJogos` ;
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Plataformas`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Plataformas` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Plataformas` (
+  `idPlataformas` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NULL,
+  PRIMARY KEY (`idPlataformas`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Jogos`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Jogos` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Jogos` (
+  `idJogos` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `preco` DOUBLE NOT NULL,
+  `idPlataformas` INT NOT NULL,
+  PRIMARY KEY (`idJogos`),
+  INDEX `fk_Jogos_Plataformas1_idx` (`idPlataformas` ASC) VISIBLE,
+  CONSTRAINT `fk_Jogos_Plataformas1`
+    FOREIGN KEY (`idPlataformas`)
+    REFERENCES `LojaDeJogos`.`Plataformas` (`idPlataformas`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Clientes`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Clientes` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Clientes` (
+  `idClientes` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `telefone` BIGINT(45) NOT NULL,
+  PRIMARY KEY (`idClientes`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Filiais`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Filiais` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Filiais` (
+  `idFiliais` INT NOT NULL AUTO_INCREMENT,
+  `cidade` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idFiliais`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Funcionarios`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Funcionarios` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Funcionarios` (
+  `idFuncionarios` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `salario` DOUBLE NULL,
+  `idFiliais` INT NOT NULL,
+  PRIMARY KEY (`idFuncionarios`),
+  INDEX `fk_Funcionarios_Filiais1_idx` (`idFiliais` ASC) VISIBLE,
+  CONSTRAINT `fk_Funcionarios_Filiais1`
+    FOREIGN KEY (`idFiliais`)
+    REFERENCES `LojaDeJogos`.`Filiais` (`idFiliais`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `LojaDeJogos`.`Vendas`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `LojaDeJogos`.`Vendas` ;
+
+CREATE TABLE IF NOT EXISTS `LojaDeJogos`.`Vendas` (
+  `idVendas` INT NOT NULL,
+  `descricao` VARCHAR(45) NOT NULL,
+  `valor` DOUBLE NULL,
+  `idClientes` INT NOT NULL,
+  `idJogos` INT NOT NULL,
+  `idFuncionarios` INT NOT NULL,
+  PRIMARY KEY (`idVendas`),
+  INDEX `fk_vendas_Clientes_idx` (`idClientes` ASC) VISIBLE,
+  INDEX `fk_vendas_jogos1_idx` (`idJogos` ASC) VISIBLE,
+  INDEX `fk_Vendas_Funcionarios1_idx` (`idFuncionarios` ASC) VISIBLE,
+  CONSTRAINT `fk_vendas_Clientes`
+    FOREIGN KEY (`idClientes`)
+    REFERENCES `LojaDeJogos`.`Clientes` (`idClientes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_vendas_jogos1`
+    FOREIGN KEY (`idJogos`)
+    REFERENCES `LojaDeJogos`.`Jogos` (`idJogos`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Vendas_Funcionarios1`
+    FOREIGN KEY (`idFuncionarios`)
+    REFERENCES `LojaDeJogos`.`Funcionarios` (`idFuncionarios`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+ */
         }
     }
 }
