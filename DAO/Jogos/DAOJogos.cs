4704f95c996bde2d3194b9ac9076692084e4e855
Vitor Claudio <vitorcldeos@gmail.com>
2019-06-10 20:54:36 -0300

    Em desenvolvimento tabela jogos

---
+++ DAO/Jogos/DAOJogos.cs
@@ -0,0 +1,33 @@
+﻿using LojadeJogo.Domain;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LojadeJogo.DAO.Jogos
+{
+    class DAOJogos
+    {
+        ConnectionFactory connection = new ConnectionFactory();
+        
+        public void Salvar(Jogo jogo)
+        {
+            try
+            {
+                connection.Conectar();
+                MySqlConnection conexao = connection.getConnection();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = "INSERT INTO jogos(nome, preco, idPlataformas) VALUES(?nome, ?preco, ?idPlataformas)";
+                cmd.Parameters.Add("?nome", MySqlDbType.VarChar).Value = jogo.nome;
+                cmd.Parameters.Add("?preco", MySqlDbType.Double).Value = jogo.preco;
+                cmd.Parameters.Add("?idPlataformas", MySqlDbType.Int32).Value = jogo.idPlataforma;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Salvo com sucesso");
+            }
+            
+        }
+    }
+}
