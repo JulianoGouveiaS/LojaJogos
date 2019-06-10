416d53e9035cc01192067f61180683ced17147c1
Lohan Alves <lohanalvesmuller@gmail.com>
2019-06-10 19:35:53 -0300

    Cadastro do funcionario

---
+++ CadFuncionarios.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace LojadeJogo
+{
+    partial class CadFuncionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.txt_Salario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bttn_Adicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(68, 19);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(353, 20);
+            this.txt_Nome.TabIndex = 1;
+            // 
+            // txt_Salario
+            // 
+            this.txt_Salario.Location = new System.Drawing.Point(68, 77);
+            this.txt_Salario.Name = "txt_Salario";
+            this.txt_Salario.Size = new System.Drawing.Size(353, 20);
+            this.txt_Salario.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Salário: ";
+            // 
+            // bttn_Adicionar
+            // 
+            this.bttn_Adicionar.Location = new System.Drawing.Point(463, 70);
+            this.bttn_Adicionar.Name = "bttn_Adicionar";
+            this.bttn_Adicionar.Size = new System.Drawing.Size(75, 23);
+            this.bttn_Adicionar.TabIndex = 4;
+            this.bttn_Adicionar.Text = "Adicionar";
+            this.bttn_Adicionar.UseVisualStyleBackColor = true;
+            this.bttn_Adicionar.Click += new System.EventHandler(this.bttn_Adicionar_Click);
+            // 
+            // CadFuncionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 118);
+            this.Controls.Add(this.bttn_Adicionar);
+            this.Controls.Add(this.txt_Salario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Nome);
+            this.Controls.Add(this.label1);
+            this.Name = "CadFuncionarios";
+            this.Text = "CadFuncionarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.TextBox txt_Salario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bttn_Adicionar;
+    }
+}
