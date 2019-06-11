89a31dd0be70991882b7248a53492a8560956cb9
Vitor Claudio <vitorcldeos@gmail.com>
2019-06-11 10:47:09 -0300

    JogosDAO editar, excluir e listar. Por enquanto sem os forms

--- DAO/Jogos/DAOJogos.cs
+++ DAO/Jogos/DAOJogos.cs
@@ -2,6 +2,7 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,103 @@ namespace LojadeJogo.DAO.Jogos
                connection.Close();
             }
             
+        }
+
+        public void Excluir(Jogo jogo)
+        {
+            try
+            { 
+            connection.Conectar();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = connection.getConnection();
+
+            cmd.CommandText = "DELETE FROM jogos WHERE idJogos = ?id";
+            cmd.Parameters.Add("?id", MySqlDbType.Int32).Value = jogo.idJogo;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+
+            }
+            catch (MySqlException ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+                MessageBox.Show("Nao deu certo a conexao!!");
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+
+        }
+
+        public void Editar(Jogo jogo)
+        {
+
+            try
+            {
+                connection.Conectar();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection.getConnection();
+                cmd.CommandText = "UPDATE jogos SET (nome = ?nome, preco = ?preco, idPlataformas = ?idPlataformas) where idJogos = ?idJogos";
+                cmd.Parameters.Add("?nome", MySqlDbType.VarString).Value = jogo.nome;
+                cmd.Parameters.Add("?preco", MySqlDbType.Double).Value = jogo.preco;
+                cmd.Parameters.Add("?idPlataformas", MySqlDbType.Int32).Value = jogo.idPlataforma;
+                cmd.Parameters.Add("?idJogos", MySqlDbType.Int32).Value = jogo.idJogo;
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (MySqlException ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+                MessageBox.Show("Nao deu certo a conexao!!");
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+        }
+
+        public DataTable Listar(Jogo jogo)
+        {
+
+            DataTable tabela = new DataTable();
+            try
+            {
+                connection.Conectar();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection.getConnection();
+                cmd.CommandText = "SELECT * FROM jogos ORDER BY idJogos";
+                MySqlDataReader leitor = cmd.ExecuteReader();
+
+                tabela.Load(leitor);
+               
+            }
+             catch(MySqlException ex)
+            {
+
+                MessageBox.Show(ex.StackTrace);
+                MessageBox.Show("Nao deu certo a conexao!!");
+
+            }
+            finally
+            {
+                
+                connection.Close();
+            }
+
+            return tabela;
+
         }
     }
 }
--- Forms/Jogo/CadJogo.Designer.cs
+++ Forms/Jogo/CadJogo.Designer.cs
@@ -102,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 170);
             this.Controls.Add(this.btn_adicionar);
             this.Controls.Add(this.comboBox_plataforma);
             this.Controls.Add(this.label3);
