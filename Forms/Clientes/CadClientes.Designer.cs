63fd2172e93932a69e3e47535472a3bab12c7928
juliano <juliano_gouveia1@hotmail.com>
2019-06-10 21:48:24 -0300

    excluir e listar clientes feito

---
+++ Forms/Clientes/CadClientes.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LojadeJogo.Forms.Clientes
+{
+    partial class CadClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Adicionar = new System.Windows.Forms.Button();
+            this.txt_Tel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Adicionar
+            // 
+            this.btn_Adicionar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btn_Adicionar.Location = new System.Drawing.Point(457, 68);
+            this.btn_Adicionar.Name = "btn_Adicionar";
+            this.btn_Adicionar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Adicionar.TabIndex = 9;
+            this.btn_Adicionar.Text = "Adicionar";
+            this.btn_Adicionar.UseVisualStyleBackColor = true;
+            this.btn_Adicionar.Click += new System.EventHandler(this.btn_Adicionar_Click);
+            // 
+            // txt_Tel
+            // 
+            this.txt_Tel.Location = new System.Drawing.Point(64, 70);
+            this.txt_Tel.Name = "txt_Tel";
+            this.txt_Tel.Size = new System.Drawing.Size(353, 20);
+            this.txt_Tel.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Telefone:";
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(64, 12);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(353, 20);
+            this.txt_Nome.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome: ";
+            // 
+            // CadClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 113);
+            this.Controls.Add(this.btn_Adicionar);
+            this.Controls.Add(this.txt_Tel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_Nome);
+            this.Controls.Add(this.label1);
+            this.Name = "CadClientes";
+            this.Text = "CadClientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Adicionar;
+        private System.Windows.Forms.TextBox txt_Tel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Nome;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Forms/Clientes/ExcCliente.cs
@@ -0,0 +1,35 @@
+﻿using LojadeJogo.DAO.Clientes;
+using LojadeJogo.DAO.Plataformas;
+using LojadeJogo.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LojadeJogo.Forms.Clientes
+{
+    public partial class ExcCliente : Form
+    {
+        Utilitarios utils = new Utilitarios();
+        DAOClientes dao = new DAOClientes();
+
+        public ExcCliente()
+        {
+            InitializeComponent();
+            utils.preencherCombo(cmbClientes, dao.lista(), "idClientes", "nome");
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            int idEscolhido = int.Parse(cmbClientes.SelectedValue.ToString());
+            dao.Excluir(idEscolhido);
+            utils.preencherCombo(cmbClientes, dao.lista(), "idClientes", "nome");
+        }
+    }
+}
